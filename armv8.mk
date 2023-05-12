6323a8cf6e4d120e68ec7c09c5e525d6eb26f0fe
骷髅头 <lasstp5011@gmail.com>
2023-05-12 15:24:39 +0800

    Update armv8.mk

--- armv8.mk
+++ armv8.mk
@@ -1,136 +1,35 @@
-# SPDX-License-Identifier: GPL-2.0-only
-#
-# Copyright (C) 2020 Tobias Maedel
-
-# FIT will be loaded at 0x02080000. Leave 16M for that, align it to 2M and load the kernel after it.
-KERNEL_LOADADDR := 0x03200000
-
-define Device/embedfire_doornet1
-  DEVICE_VENDOR := EmbedFire
-  DEVICE_MODEL := DoorNet1
-  SOC := rk3328
-  UBOOT_DEVICE_NAME := doornet1-rk3328
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r2s | pine64-bin | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-usb-net-rtl8152 kmod-rtl8821cu
-endef
-TARGET_DEVICES += embedfire_doornet1
-
-define Device/embedfire_doornet2
-  DEVICE_VENDOR := EmbedFire
-  DEVICE_MODEL := DoorNet2
-  SOC := rk3399
-  UBOOT_DEVICE_NAME := doornet2-rk3399
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r4s | pine64-img | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-r8168 -urngd
-endef
-TARGET_DEVICES += embedfire_doornet2
-
-define Device/embedfire_lubancat1
-  DEVICE_VENDOR := EmbedFire
-  DEVICE_MODEL := LubanCat1
-  SOC := rk3566
-  UBOOT_DEVICE_NAME := lubancat1-rk3566
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r5s | pine64-img | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-r8125
-endef
-TARGET_DEVICES += embedfire_lubancat1
-
-define Device/embedfire_lubancat1n
-  DEVICE_VENDOR := EmbedFire
-  DEVICE_MODEL := LubanCat1N
-  SOC := rk3566
-  UBOOT_DEVICE_NAME := lubancat1n-rk3566
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r5s | pine64-img | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-r8169 -urngd kmod-ata-ahci
-endef
-TARGET_DEVICES += embedfire_lubancat1n
-
-define Device/embedfire_lubancat2
-  DEVICE_VENDOR := EmbedFire
-  DEVICE_MODEL := LubanCat2
-  SOC := rk3568
-  UBOOT_DEVICE_NAME := lubancat2-rk3568
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r5s | pine64-img | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-ata-ahci kmod-ata-ahci-platform kmod-ata-core kmod-ata-ahci kmod-ata-ahci-platform kmod-ata-core
-endef
-TARGET_DEVICES += embedfire_lubancat2
-
-define Device/embedfire_lubancat2n
-  DEVICE_VENDOR := EmbedFire
-  DEVICE_MODEL := LubanCat2N
-  SOC := rk3568
-  UBOOT_DEVICE_NAME := lubancat2n-rk3568
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r5s | pine64-img | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-r8125 kmod-ata-ahci kmod-ata-ahci-platform kmod-ata-core
-endef
-TARGET_DEVICES += embedfire_lubancat2n
-
-define Device/hinlink_opc-h68k
-  DEVICE_VENDOR := HINLINK
-  DEVICE_MODEL := OPC-H68K
-  SOC := rk3568
-  UBOOT_DEVICE_NAME := opc-h68k-rk3568
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r5s | pine64-img | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-r8125
-endef
-TARGET_DEVICES += hinlink_opc-h68k
-
-define Device/friendlyarm_nanopi-r2c
-  DEVICE_VENDOR := FriendlyARM
-  DEVICE_MODEL := NanoPi R2C
-  SOC := rk3328
-  UBOOT_DEVICE_NAME := nanopi-r2c-rk3328
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r2s | pine64-bin | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-usb-net-rtl8152  kmod-rtl8821cu
-endef
-TARGET_DEVICES += friendlyarm_nanopi-r2c
-
-define Device/friendlyarm_nanopi-r2s
-  DEVICE_VENDOR := FriendlyARM
-  DEVICE_MODEL := NanoPi R2S
-  SOC := rk3328
-  UBOOT_DEVICE_NAME := nanopi-r2s-rk3328
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r2s | pine64-bin | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-usb-net-rtl8152
-endef
-TARGET_DEVICES += friendlyarm_nanopi-r2s
-
-define Device/friendlyarm_nanopi-r4s
-  DEVICE_VENDOR := FriendlyARM
-  DEVICE_MODEL := NanoPi R4S
-  SOC := rk3399
-  UBOOT_DEVICE_NAME := nanopi-r4s-rk3399
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r4s | pine64-bin | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-r8168  kmod-rtl8821cu -urngd
-endef
-TARGET_DEVICES += friendlyarm_nanopi-r4s 
-
-define Device/friendlyarm_nanopi-r4se
-  DEVICE_VENDOR := FriendlyARM
-  DEVICE_MODEL := NanoPi R4SE
-  SOC := rk3399
-  UBOOT_DEVICE_NAME := nanopi-r4se-rk3399
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r4s | pine64-bin | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-r8168 -urngd
-endef
-TARGET_DEVICES += friendlyarm_nanopi-r4se
-
-define Device/friendlyarm_nanopi-r5s
-  DEVICE_VENDOR := FriendlyARM
-  DEVICE_MODEL := NanoPi R5S
-  SOC := rk3568
-  UBOOT_DEVICE_NAME := nanopi-r5s-rk3568
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r5s | pine64-img | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-r8125 kmod-ata-ahci kmod-ata-ahci-platform kmod-ata-core
-endef
-TARGET_DEVICES += friendlyarm_nanopi-r5s
-
-define Device/friendlyarm_nanopi-r5c
-  DEVICE_VENDOR := FriendlyARM
-  DEVICE_MODEL := NanoPi R5C
-  SOC := rk3568
-  UBOOT_DEVICE_NAME := nanopi-r5c-rk3568
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r5s | pine64-img | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-r8125 kmod-ata-ahci kmod-ata-ahci-platform kmod-ata-core
-endef
-TARGET_DEVICES += friendlyarm_nanopi-r5c
+[  552.192850] rtk_btusb: btchr_open: Device not probed
+[  552.193066] rtk_btusb: btchr_open: BT usb char device is opening
+[  552.193118] rtk_btusb: btchr_open: Device not probed
+[  552.193334] rtk_btusb: btchr_open: BT usb char device is opening
+[  552.193386] rtk_btusb: btchr_open: Device not probed
+[  552.193604] rtk_btusb: btchr_open: BT usb char device is opening
+[  552.193657] rtk_btusb: btchr_open: Device not probed
+[  552.457174] type=1400 audit(1683876245.625:577): avc: denied { open } for comm="crash_dump64" path="/dev/__properties__/u:object_r:hwservicemanager_prop:s0" dev="tmpfs" ino=18355 scontext=u:r:crash_dump:s0 tcontext=u:object_r:hwservicemanager_prop:s0 tclass=file permissive=1
+[  552.457361] type=1400 audit(1683876245.625:578): avc: denied { getattr } for comm="crash_dump64" path="/dev/__properties__/u:object_r:hwservicemanager_prop:s0" dev="tmpfs" ino=18355 scontext=u:r:crash_dump:s0 tcontext=u:object_r:hwservicemanager_prop:s0 tclass=file permissive=1
+[  552.458211] audit: audit_lost=335 audit_rate_limit=5 audit_backlog_limit=64
+[  552.458228] audit: rate limit exceeded
+[  553.004240] init: Untracked pid 3116 exited with status 0
+[  553.019887] init: Untracked pid 3118 exited with status 0
+[  553.041537] binder: release 3059:3101 transaction 184748 in, still active
+[  553.041569] binder: release 3059:3101 transaction 184414 out, still active
+[  553.041579] binder: send failed reply for transaction 184748 to 3053:3053
+[  553.041841] binder: 3053:3053 transaction failed 29189/0, size 4-0 line 2995
+[  553.041886] binder: send failed reply for transaction 184414, target dead
+[  553.756158] audit: audit_lost=356 audit_rate_limit=5 audit_backlog_limit=64
+[  553.756203] audit: rate limit exceeded
+[  553.817793] init: Untracked pid 3128 exited with status 0
+[  553.818888] init: Service 'vendor.bluetooth-1-0' (pid 3053) received signal 11
+[  553.818975] init: Sending signal 9 to service 'vendor.bluetooth-1-0' (pid 3053) process group...
+[  553.825173] libprocessgroup: Successfully killed process cgroup uid 1002 pid 3053 in 5ms
+[  553.826483] init: Untracked pid 3130 exited with status 0
+[  553.827493] init: starting service 'vendor.bluetooth-1-0'...
+[  555.596235] rtk_btusb: btchr_open: BT usb char device is opening
+[  555.596308] rtk_btusb: btchr_open: Device not probed
+[  555.596543] rtk_btusb: btchr_open: BT usb char device is opening
+[  555.596586] rtk_btusb: btchr_open: Device not probed
+[  555.596761] rtk_btusb: btchr_open: BT usb char device is opening
+[  555.596799] rtk_btusb: btchr_open: Device not probed
+[  555.596956] rtk_btusb: btchr_open: BT usb char device is opening
+[  555.596977] rtk_btusb: btchr_open: Device not probed
+[  555.597142] rtk_btusb: btchr_open: BT usb char device is opening
